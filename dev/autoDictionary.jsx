d0078eb3cf169e34b39d3ffee6e71f0434bb5d29
Evelyn Lee <evelynlee34@gmail.com>
2018-01-23 15:05:38 -0400

    Adjust widths and add some code

--- dev/autoDictionary.jsx
+++ dev/autoDictionary.jsx
@@ -15,22 +15,40 @@ let autoDictionary = React.createClass({
     };
   },
 
+  createChoices(definition) {
+    let words = definition.split(" ");
+    let output = words.map((word, idx) => {
+      console.log(this);
+      return (
+        <span key={`${word}-${idx}`}
+              onClick={this.handleLookupClick(word)}
+        >
+        {word}
+      </span>);
+    })
+    console.log(output);
+  },
+
   handleChange(property, event) {
     this.setState({word: event.target.value}, this.handleLookup(event.target.value));
   },
 
   handleLookup(word) {
     let upWord = word.toUpperCase();
+    let definition = this.state.dict[upWord];
 
-    if (this.state.dict[upWord] !== undefined) {
-      this.setState({
-        definition: this.state.dict[upWord],
-      });
+    if (definition !== undefined) {
+      this.setState({ definition: definition });
+      // this.createChoices(definition);
     } else {
       this.setState({
         definition: "",
       })
     }
+  },
+
+  handleLookupClick(word) {
+    console.log("in handleLookupClick");
   },
 
   handleSubmit(event) {
@@ -41,7 +59,12 @@ let autoDictionary = React.createClass({
     return (
       <div className="main">
         <form className="dict-form" onSubmit={this.handleSubmit}>
-          Word: <input type="text" value={this.state.word} onChange={this.handleChange.bind(this, "word")} className="dict-form-word"/><br/>
+          Word: <input type="text"
+                       value={this.state.word}
+                       onChange={this.handleChange.bind(this, "word")}
+                       className="dict-form-word"
+                       pattern="[A-Za-z]*"
+                       />
         </form>
 
         <div className="dict-def">
