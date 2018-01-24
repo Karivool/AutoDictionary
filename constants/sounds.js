0dca508200737e11a875676dcef84491effd903b
Evelyn Lee <evelynlee34@gmail.com>
2018-01-24 08:46:56 -0400

    Add sound effects

---
+++ constants/sounds.js
@@ -0,0 +1,10 @@
+const SoundFX = [
+  "./sounds/type1.mp3",
+  "./sounds/type2.mp3",
+  "./sounds/type3.mp3",
+  "./sounds/type4.mp3",
+  "./sounds/type5.mp3",
+  "./sounds/type6.mp3",
+]
+
+module.exports = SoundFX;
